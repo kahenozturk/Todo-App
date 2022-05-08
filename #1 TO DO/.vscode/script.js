e77f410c90ab391c7f47200e6d3f0f654af339d1
kahenozturk <kahenozturk@gmail.com>
2022-05-08 15:53:19 +0300

    Delete an Item
    In this commit I add a new feature which is deleting item from the to do list.

--- #1 TO DO/.vscode/script.js
+++ #1 TO DO/.vscode/script.js
@@ -1,52 +1,85 @@
+// UI vars
 
-// UI vars 
-
-const form = document.querySelector('form');
-const input = document.querySelector('#txtTaskName');
-const btnDeleteAll = document.querySelector('#btnDeleteAll');
-const taskList = document.querySelector('#task-list');
-
+const form = document.querySelector("form");
+const input = document.querySelector("#txtTaskName");
+const btnDeleteAll = document.querySelector("#btnDeleteAll");
+const taskList = document.querySelector("#task-list");
 
 // call event listeners
 eventListeners();
 
-function eventListeners(){
-    // submit event
-    form.addEventListener('submit',addNewItem);
+function eventListeners() {
+  // submit event
+  form.addEventListener("submit", addNewItem);
+
+  //delete an item
+  taskList.addEventListener("click", deleteItem);
+
+  //delete all Items
+  btnDeleteAll.addEventListener("click", deleteAllItems);
 }
 
 // add new item
-function addNewItem(e){
-   if(input.value == ''){
-       alert('add new item');
+function addNewItem(e) {
+  if (input.value == "") {
+    alert("add new item");
+  }
+
+  // create li
+  const li = document.createElement("li");
+  li.className = "list-group-item list-group-item-secondary";
+  li.appendChild(document.createTextNode(input.value));
+
+  //create a
+  const a = document.createElement("a");
+
+  a.classList = "delete-item float-right";
+  a.setAttribute("href", "#");
+  a.innerHTML = ' <i class="fas fa-times"></i>';
+
+  // add a to li
+  li.appendChild(a);
+
+  //add li to ul
+  taskList.appendChild(li);
+
+  input.value = "";
+
+  console.log(li);
+
+  e.preventDefault();
 }
 
+//delete an item
+function deleteItem(e) {
+  if (e.target.className === "fas fa-times") {
+    e.target.parentElement.parentElement.remove();
+  }
 
-// create li
-const li = document.createElement('li');
-li.className='list-group-item list-group-item-secondary';
-li.appendChild(document.createTextNode(input.value));
+  e.preventDefault();
+}
 
+//delete all Items
+function deleteAllItems(e) {
+  // 1. Yöntem!!!
+  // if(confirm('Are you sure?')){
+  //     taskList.innerHTML='';
+  //     }
 
+  //2.Yöntem
+  // if('click')
+  // {
+  //     taskList.innerHTML='';
+  // }
 
-//create a
-const a = document.createElement('a');
+  //3.Yöntem
+  if (confirm("Are you sure!")) {
+    taskList.childNodes.forEach(function (item) {
+      if (item.nodeType === 1) {
+        item.remove();
+      }
+    });
+  }
 
-a.classList='delete-item float-right';
-a.setAttribute('href','#');
-a.innerHTML=' <i class="fas fa-times"></i>';
-
-// add a to li
-li.appendChild(a);
-
-//add li to ul
-taskList.appendChild(li);
-
-input.value='';
-
-console.log(li);
-
-
-    e.preventDefault();
-
+  e.preventDefault();
 }
